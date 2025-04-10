9395ab1d709a10ab301ce62ead2187100e2323c7
rahul <nizarerahul7@gmail.com>
2025-04-10 22:40:51 +0530

    lazy laoding to optimize app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # React-practice-work
+
+## optimazation Technique - also know as:
+
+code slipting
+chunking
+dyanamic import
+lazy loading
+demand loading
+
+- use lazy() - a react method
+  -- lazy(()=> import('/path'))
+- wrap the component with suspense and pass a fallback prop
+## modular code using single responsibility principle
+ -- use custome hooks to sparate task
+ -- component code should be modular and clean
--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 // import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import UserClass from "./components/UserClass";
 import { createRoot } from "react-dom/client";
-import UserClass from "./components/UserClass";
+import { lazy, Suspense } from "react";
 
-function App(){
-    return (
-        <>
-        <UserClass name='Rahul Nizare' location='surat'/>
-        </>
-    )
+function App() {
+  // const arr = [1,2,3]
+  // const btn =[]
+  // debugger
+  // for(var i=0; i<arr.length; i++){
+  //     // console.log(i)
+  //     // btn.push(<button key={i} onClick={()=>alert('alert'+String(arr[i]))}>button {arr[i]}</button>)
+  //     // console.log(arr[i])
+  //     (function() {
+  //         var copy = i; // copies current value of i (e.g. 0, 1, 2)
+  //         btn.push(<button key={copy} onClick={() => alert(arr[copy])}>Button</button>);
+  //       })();
+
+  // }
+  const UserClass = lazy(() => import("./components/UserClass"));
+  return (
+    <>
+      {/* {btn} */}
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <UserClass name="Rahul Nizare" location="surat" />
+      </Suspense>
+    </>
+  );
 }
 
-const root = createRoot(document.getElementById('root'))
-root.render(
-    <App/>
-)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
